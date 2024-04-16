b31b6dd77513411a5e819b0edfc29f9035efc62a
IbrahimElyyan <ibrahimelyyan17@gmail.com>
2024-04-16 17:35:46 -0400

    consistent dark mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
+    if (element.classList.contains("dark-mode")) {
+        // Save the user's preference to localStorage.
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.removeItem('dark-mode');
+    }
+}
